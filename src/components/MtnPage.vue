bf1d293001d45c7d7d968697595905ae4b8f0a18
John Knotts <jhnnyk@gmail.com>
2020-12-20 08:14:21 -0700

    display forecast

--- src/components/MtnPage.vue
+++ src/components/MtnPage.vue
@@ -3,6 +3,26 @@
     <div v-if="selectedMtn">
       <h1>{{ selectedMtn.name }}</h1>
     </div>
+
+    <div class="forecast">
+      <h2>{{ selectedMtn.name }} Weather</h2>
+      <div v-if="forecastLoading">
+        ... loading weather forecast ...
+      </div>
+      <div v-else>
+        <div v-for="(period, index) in forecast" :key="index">
+          {{ period.name }}:
+          <span v-if="period.isDaytime">High: </span>
+          <span v-else>Low: </span>
+          {{ period.temperature }}º{{ period.temperatureUnit }}
+          {{ period.temperatureTrend }}<br />
+          {{ period.shortForecast }}<br />
+          <img :src="period.icon" :alt="period.shortForecast" /><br />
+          {{ period.detailedForecast }}
+        </div>
+        {{ forecast }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,6 +35,8 @@ export default {
   computed: {
     ...mapState({
       selectedMtn: (state) => state.selectedMtn,
+      forecast: (state) => state.forecast,
+      forecastLoading: (state) => state.forecastLoading,
     }),
   },
 };
--- src/store.js
+++ src/store.js
@@ -8,6 +8,8 @@ export const store = new Vuex.Store({
   state: {
     mtns: [],
     selectedMtn: null,
+    forecast: [],
+    forecastLoading: false,
   },
 
   getters: {
@@ -34,6 +36,12 @@ export const store = new Vuex.Store({
     },
     setSelectedMtn: (state, mtn) => {
       state.selectedMtn = mtn;
+    },
+    setForecast: (state, forecast) => {
+      state.forecast = forecast;
+    },
+    setForecastLoading: (state, val) => {
+      state.forecastLoading = val;
     },
   },
 
@@ -63,7 +71,9 @@ export const store = new Vuex.Store({
       commit('setSelectedMtn', selectedMtn);
 
       if (selectedMtn) {
+        commit('setForecastLoading', true);
         dispatch('getForecastURL', selectedMtn);
+        dispatch('getForecast', selectedMtn);
       }
     },
 
@@ -74,7 +84,14 @@ export const store = new Vuex.Store({
       const data = await response.json();
       const forecastURL = await data.properties.forecast;
 
-      // update DB with forecast URL
+      // update DB with forecast URL if needed
+      // ----------------------------------
+      // if the DB does need updated with a new forecastURL,
+      // the below will run and generate an error
+      // because of firebase permissions
+      // I'm leaving it like this because per the API docs at:
+      // https://weather-gov.github.io/api/general-faqs
+      // it should rarely need updating.
       if (forecastURL !== mtn.forecastURL) {
         const spotRef = db.collection('spots').doc(mtn.id);
         try {
@@ -84,10 +101,15 @@ export const store = new Vuex.Store({
         } catch (error) {
           console.log(error);
         }
-        console.log('forecast URL updated!');
-      } else {
-        console.log('all set!');
       }
+    },
+
+    getForecast: async ({ commit }, mtn) => {
+      const response = await fetch(mtn.forecastURL);
+      const data = await response.json();
+      const forecast = await data.properties.periods;
+      commit('setForecast', forecast);
+      commit('setForecastLoading', false);
     },
   },
 });
